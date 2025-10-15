8e141095b071319a767bd2b84ab2105710003c38
NoorAlden <NoorAldenBn3@gmail.com>
2025-10-16 00:09:23 +0300

    add message on empty favorite list

--- src/pages/favorites/index.jsx
+++ src/pages/favorites/index.jsx
@@ -6,6 +6,13 @@ export default function Favorites() {
   const { favorites, setFavorites } = useContext(GlobalContext);
   return (
     <div className="container flex flex-wrap gap-5">
+      {favorites.length < 1 ? (
+        <p className="text-2xl font-extrabold text-center mx-auto mt-8">
+          there is no favorite recipes yet.
+        </p>
+      ) : (
+        ""
+      )}
       {favorites.map((favorite) => {
         return <RecipeItem key={favorite.recipe.id} item={favorite.recipe} />;
       })}
